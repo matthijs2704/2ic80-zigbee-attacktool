c2d48f7c1b7e989c73ffeb0e8b9fdde3957220bd
matthijs2704 <matthijs.logemann@xs4all.nl>
2020-04-12 21:51:27 +0200

    Include updated header logo

---
+++ zigdiggity/interface/components/logo.py
@@ -0,0 +1,34 @@
+from zigdiggity.interface.colors import Color
+
+
+class Logo():
+
+    def __init__(self):
+        pass
+
+    def print(self):
+        Color.pl(
+            "{R}                                                           .####                ")
+        Color.pl(
+            "{R}#%%%%%%%#%%%%%%%#%%%%%  /%%%%%#          %%%%%#            %%%%                 ")
+        Color.pl(
+            "{R}#%%%%%%%%%%%%%%%%%%%%%  /%%%%%%          %%%%%#           %%%#                  ")
+        Color.pl("{R}.,,,,,,,%%%%%%*,,,,,,,  /%%%%%%          %%%%%#          %%%%                              {W}Ikea Tradfri Hacking tool based on {W}Zig{R}Diggity")
+        Color.pl("{R}        %%%%%%,         /%%%%%%          %%%%%#         %%%%       #%%%%%%.                {W}Lab on Offensive Computer Security (2IC80)")
+        Color.pl("{R}        %%%%%%,         /%%%%%%          %%%%%#        %%%%    ,%%%%%%%##%%%%%             {W}Created by: Group 102")
+        Color.pl(
+            "{R}        %%%%%%,         /%%%%%%          %%%%%#       %%%%    %%%%%       (%%%%(")
+        Color.pl(
+            "{R}        %%%%%%,         /%%%%%%          %%%%%#      %%%%    %%%%%%%%%%%%%%#%%%%           {W}[+] Max Opperman")
+        Color.pl(
+            "{R}        %%%%%%,         ,%%%%%%         .%%%%%%     #%%%     %%%%%%%%%%%%%%%%%%%           {W}[+] Matthijs Logemann")
+        Color.pl(
+            "{R}        %%%%%%,          %%%%%%%%      %%%%%%%(   .#%%%      ,%%%#                         {W}[+] Jasper Dellaert")
+        Color.pl(
+            "{R}        %%%%%%,           ,%#%%%%%%%%%%%%%%%%    .%%%%         #%%%%%%%%%%%%%##,")
+        Color.pl(
+            "{R}        #%%%%%,              /%%%%%%%%####      .%%%#            *#%%%%%#%##/   ")
+        Color.pl(
+            "{R}                                               .#%%%                            ")
+        Color.pl("{W}")
+        Color.pl("{W}")
